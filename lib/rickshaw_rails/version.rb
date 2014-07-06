a7c4c625d323ed741e958328688d27cf042dedb3
Geoffrey Roguelon <geoffrey.roguelon@gmail.com>
2014-07-06 18:48:31 +0200

    Update Rickshaw to 1.5.0.

--- lib/rickshaw_rails/version.rb
+++ lib/rickshaw_rails/version.rb
@@ -3,8 +3,8 @@
 module RickshawRails
   module Version
     MAJOR = 1
-    MINOR = 4
-    TINY  = 6
+    MINOR = 5
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
